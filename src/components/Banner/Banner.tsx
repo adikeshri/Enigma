d87e76b9ae7befd0e455680b08b31ea5caa78e87
Aditya Keshri <keshri.aditya999@gmail.com>
2021-01-02 14:59:47 +0530

    Banner changes

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import  "./Banner.css";
-import BannerLogo from "../../icons/BannerLogo.png";
+import BannerLogo from "../../icons/GreenSquare.png";
 
 class Banner extends React.Component{
 
@@ -8,7 +8,8 @@ class Banner extends React.Component{
     {
         return(
             <div className="BannerBackground">
-                <img src={BannerLogo} className="BannerImage" alt=""/>
+                <img src={BannerLogo} width="15px" height="15px" style={{margin:5, marginTop: 10.5, float: "left", marginRight: 4}}/>
+                <p className="BannerFont">ENIGMA</p>
             </div>
         )
     }
